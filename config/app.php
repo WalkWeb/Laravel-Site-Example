4d55267f66c1ca1109a2a9ccc82b026b83468778
WalkWeb <av18775@list.ru>
2018-11-12 12:59:33 +0300

    Добавлен пакет barryvdh/laravel-ide-helper для корректной подсветки классов и методов

--- config/app.php
+++ config/app.php
@@ -164,9 +164,11 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
