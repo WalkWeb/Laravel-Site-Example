d9523ac77df02cb2ab1d0d39515149bdc2a5082a
WalkWeb <av18775@list.ru>
2018-11-19 13:22:30 +0300

    Поправлена ошибка в шаблоне создания поста

--- resources/views/admin/posts/partials/categories.blade.php
+++ resources/views/admin/posts/partials/categories.blade.php
@@ -1,5 +1,5 @@
 @foreach ($categories as $category)
-    @if ($category->id === $post->category_id)
+    @if (!empty($post) && ($category->id === $post->category_id))
     <option value="{{$category->id}}" selected="selected">{{$category->title}}</option>
     @else
         <option value="{{$category->id}}">{{$category->title}}</option>
