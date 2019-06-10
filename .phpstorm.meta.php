90f18e78edc1b65a94f1d192beeaa683aeb123c0
WalkWeb <av18775@list.ru>
2019-06-10 12:42:38 +0300

    Обновлены phpDoc у моделей

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-06-10 08:56:49.
+    * Generated on 2019-06-10 09:37:04.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Post
+ *
+ * @property int $id
+ * @property string $title
+ * @property int $category_id
+ * @property string|null $desc_short
+ * @property string $desc
+ * @property string|null $meta_title
+ * @property string|null $meta_desc
+ * @property string|null $meta_key
+ * @property int|null $published
+ * @property int $view
+ * @property int $created_by
+ * @property int|null $edit_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post lastPosts($count)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post published()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereDesc($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereDescShort($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereEditBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereMetaDesc($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereMetaKey($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereMetaTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post wherePublished($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Post whereView($value)
+ * @mixin \Eloquent
+ */
 class Post extends Model
 {
     /**
